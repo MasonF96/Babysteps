e3221ed9840cb09a5af274de394302c80ab683e5
mason <mason@172.16.240.234>
2020-01-30 08:30:02 -0600

    Trial run

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mason
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Class");
+        System.out.println("Hello class2");
+        
+        System.out.print("This is not /n");
+        System.out.print("Be printed on one line");
+    }
+      
+}
